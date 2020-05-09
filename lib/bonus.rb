c64d701f12fece2c631c67a53abe8d53ed77d013
Heath Mast <hrmast@gmail.com>
2020-05-09 02:16:41 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,10 @@ def bonus
   }
 
   epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
-end
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
+
+  puts epic_tragedy
+end
+  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  
